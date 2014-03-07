2ebb2842ead72b2b93c0f76e2ea974b934eea157
Jacob Wejendorp <jacob@wejendorp.dk>
2014-03-07 15:53:21 +0100

    Test rigging

--- test/spec/client.js
+++ test/spec/client.js
@@ -2,6 +2,7 @@
 var chai = require('chai');
 
 var Client = require('../../lib/client');
+var protocol = require('../../lib/protocol/protobuf')('client');
 
 var port = 6666;
 var host = 'localhost';
@@ -11,6 +12,7 @@ describe('client', function() {
     client = new Client('test');
     client.init({host: host, port: port}, done);
   });
+
   after(function(done) {
     client.close(done);
   });
@@ -19,17 +21,85 @@ describe('client', function() {
     chai.assert(client.connection);
     chai.assert(client.nodes.length >= 1);
   });
-  it('can put a key', function(done) {
-    client.put('chocolate', 'yuck', function(err, res) {
-      if(err) return done(err);
-      done();
+
+
+  describe('#put', function() {
+    var version;
+    it('succeeds for non-versioned', function(done) {
+      client.put('chocolate', 'yum', function(err, result) {
+        chai.assert(version = result.version);
+        done();
+      });
+    });
+
+    it('fails for outdated version', function(done) {
+      client.put('chocolate', 'yum', function(err, result) {
+        if(err) return done(err);
+
+        client.put('chocolate', 'yuck', {version: result.version}, function(err) {
+          chai.assert(err, 'Put should fail');
+          done();
+        });
+      });
     });
   });
-  it('can get a key', function(done) {
-    client.get('chocolate', function(err, res) {
+
+  function getNull(key, done) {
+    client.get(key, function(err, res) {
       if(err) return done(err);
-      chai.assert(res.value.toString() === 'yuck');
+
+      chai.assert(res === null);
       done();
+    });
+  }
+
+  describe('#get', function() {
+    var version;
+    before(function initKey(done) {
+      client.put('chocolate', 'yum', function(err, result) {
+        version = result.version;
+        done();
+      });
+    });
+
+    it('retrieves value', function(done) {
+      client.get('chocolate', function(err, res) {
+        if(err) return done(err);
+        chai.assert(res.value.toString() === 'yum');
+        done();
+      });
+    });
+    it('returns null on non-existing key', function(done) {
+      getNull('cookieMonster', done);
+    });
+  });
+
+
+  describe.skip('#del', function() {
+    var version;
+    before(function initKey(done) {
+      client.put('chocolate', 'yum', function(err, result) {
+        version = result.version;
+        done();
+      });
+    });
+
+    it('removes a key', function(done) {
+      client.del('chocolate', version, function(err) {
+        if(err) return done(err);
+
+        getNull('chocolate', done);
+      });
+    });
+  });
+
+
+  describe.skip('#reconnect', function() {
+    it('reconnects after 500 requests', function(done) {
+      done(new Error('not implemented'));
+    });
+    it('reconnects on error??', function() {
+      done(new Error('not implemented'));
     });
   });
 });
